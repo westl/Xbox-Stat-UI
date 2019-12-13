d2e6036c6f83a280d12ff08dbfce26e5ca605a87
lamar west <westl@uci.edu>
2019-12-12 23:35:32 -0800

    fixed unit test

--- src/components/ProfileDisplay.test.tsx
+++ src/components/ProfileDisplay.test.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { ProfileDisplay } from './ProfileDisplay';
-import sampleProfileData from '../test_data';
+import sampleProfileData from '../test_data/xbox-profile.json';
 describe('ProfileDisplay component', () => {
   it('should render without crashing', () => {
     const div = document.createElement('div');
@@ -11,10 +11,10 @@ describe('ProfileDisplay component', () => {
     ReactDOM.unmountComponentAtNode(div);
   });
 
-  it('should render xbox profile data', () => {
+  it('should not show initial message, should render xbox profile data if profile is given', () => {
     const initialMessage = 'Enter a profile to see its data';
     const { getByTestId } = render(<ProfileDisplay initialMessage={initialMessage} profile={sampleProfileData} />);
     
-    expect(getByTestId(/initialMessage/i).textContent).toBe(initialMessage)
+    expect(getByTestId(/initialMessage/i).textContent).toBe('')
   });
 });
--- src/components/ProfileDisplay.tsx
+++ src/components/ProfileDisplay.tsx
@@ -4,21 +4,31 @@ export interface ProfileDisplayProps {
   initialMessage?: string;
   profile?: any;
 }
-export const ProfileDisplay: React.FC<ProfileDisplayProps> = ({ initialMessage, profile }) => {
-  console.log(profile);
+export const ProfileDisplay: React.FC<ProfileDisplayProps> = ({
+  initialMessage,
+  profile
+}) => {
+  if (!profile) return null;
   const messageToDisplay = !profile && initialMessage;
   return (
     <div className="profile-display">
       <div className="row">
         <div className="col-sm-12">
-          <div className="row" style={{ backgroundColor: `#${profile.preferredColor.primaryColor}` }}>
+          <div
+            className="row"
+            style={{
+              backgroundColor: `#${profile.preferredColor.primaryColor}`
+            }}
+          >
             <div className="col-sm-12 col-md-4">
-              {messageToDisplay}
+              <div data-testid="initialMessage">{messageToDisplay}</div>
               <div className="pic-container my-3">
                 <img className="avatar" src={profile.displayPicRaw} />
               </div>
             </div>
-            <div className="col-sm-12 col-md-4">Achievements and Gamerscore</div>
+            <div className="col-sm-12 col-md-4">
+              Achievements and Gamerscore
+            </div>
             <div className="col-sm-12 col-md-4">Current game details</div>
           </div>
         </div>
